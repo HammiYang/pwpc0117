e601b2ae16488b36415fe0e8d974ee7a6a09e222
Hammi Muñiz <Hammi Muiz>
2017-05-08 22:12:48 -0500

    Mis cambios en Heroku

--- config/config.js
+++ config/config.js
@@ -1,11 +1,17 @@
+//exporta el objeto module.exports
 module.exports = {
-    "IP" : '127.0.0.1' || process.env.IP,
-    "PORT" : 3000 || process.env.PORT,
-    "color_theme" : {
-        "info" : "rainbow",
-        "data" : "green",
-        "error" : "red",
-        "warning" : "yellow"
-    },
-    "STATIC_PATH" : "./static"
+   //"IP":'127.0.0.1' || process.env.IP,
+    //"IP": process.env.IP || '127.0.0.1',
+   "IP": process.env.IP || '0.0.0.0',
+  "PORT":process.env.PORT || '3000',
+     // "IP": process.env.IP, 
+  // "PORT":process.env.PORT,
+    "color_theme": {
+        "info":"rainbow",
+        "data":"green",
+        "error":"red",
+        "warning":"yellow"
+    },//estatico
+    "STATIC_PATCH":"./static",
+    "dbStringConnection":process.env.DB || "mongodb://calaca:1234@ds133261.mlab.com:33261/inventarios"
 };
